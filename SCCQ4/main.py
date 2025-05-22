a0701d163690d54170a024662e85b1dd308f1b32
GameCobra <adrian@houser.ca>
2025-05-22 14:02:18 -0400

    SCC question 4 done

--- SCCQ4/main.py
+++ SCCQ4/main.py
@@ -1,6 +1,8 @@
-S = 3 #Size of maze
+S = 5 #Size of maze
 N = 3 #Number of bananas stolen
 B = 5 #Number of bananas on banana tile
+
+exitAmount = -1
 
 def SplitString(value : str, amount : int):
     outList = []
@@ -10,10 +12,19 @@ def SplitString(value : str, amount : int):
             outList[i % amount].append(value[i])
     return outList
 
-grid = SplitString("OEWMWSOGO", S)
+grid = SplitString("OOEWSMGWOOMWGGOGOMWGMWOGM", S)
 # OEW
 # MWS
 # OGO
+
+def FindStart():
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == "S":
+                return [i, j]
+
+# {Bananas: , path: []}
+toDo = [{"Positiion": FindStart(),"Bananas" : 0, "Path" : []}]
 
 def GetAdjacentTiles(x : int, y : int):
     returnList = []
@@ -28,16 +39,40 @@ def GetAdjacentTiles(x : int, y : int):
         returnList.append([grid[x - 1][y], x - 1, y])
     return returnList
 
-adjacentTiles = GetAdjacentTiles(2, 1)
-i = 0
-while (True):
-    if adjacentTiles[i][0] == "W":
-        adjacentTiles.pop(i)
-        i -= 1
-    i += 1
-    if i >= (len(adjacentTiles)):
-        break
+def GetGoodAdjacentTiles(x : int, y : int):
+    adjacentTiles = GetAdjacentTiles(x, y)
+    i = 0
+    while (True):
+        if adjacentTiles[i][0] == "W":
+            adjacentTiles.pop(i)
+            i -= 1
+        i += 1
+        if i >= (len(adjacentTiles)):
+            break
+    return adjacentTiles
+
+def execute():
+    global exitAmount
+    element = toDo.pop()
+    adjEl = GetGoodAdjacentTiles(*element["Positiion"])
+    for i in range(len(adjEl)):
+        if [adjEl[i][1], adjEl[i][2]] in element["Path"]:
+            continue
+        path = element["Path"]
+        path.append(element["Positiion"])
+        if adjEl[i][0] == "G":
+            toDo.append({"Positiion": [adjEl[i][1], adjEl[i][2]],"Bananas" : element["Bananas"] + B, "Path" : path})
+        if adjEl[i][0] == "M":
+            if element["Bananas"] - N < 0:
+                continue
+            toDo.append({"Positiion": [adjEl[i][1], adjEl[i][2]],"Bananas" : element["Bananas"] - N, "Path" : path})
+        if adjEl[i][0] == "O":
+            toDo.append({"Positiion": [adjEl[i][1], adjEl[i][2]],"Bananas" : element["Bananas"], "Path" : path})
+        if adjEl[i][0] == "E":
+            exitAmount = max(exitAmount, element["Bananas"])
 
 
 
-print(adjacentTiles)
+while (len(toDo) != 0):
+    execute()
+print(exitAmount)
